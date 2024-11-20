b30b3bccc6917fa1e8268bf4cc08bcd4860f319c
ICEMNN <seangroves9@gmail.com>
2024-11-20 18:21:45 -0500

    finalize login/register authentication

--- Registervalid.js
+++ Registervalid.js
@@ -4,20 +4,32 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+
+function stringify(item) {
+    return JSON.stringify(item);
+}
+
+
 document.getElementById('form').addEventListener('submit', function(event) {
 	event.preventDefault(); 
-
-	/*
-	const trn = document.getElementById('trn');
-	const email = document.getElementById('email');
-	const password = document.getElementById('password');
-	const form = document.getElementById("form");
-	*/
+	checkInputs();
 	
-	localStorage.setItem('Trn', trn);
-	localStorage.setItem('email', email);
-	localStorage.setItem('password', password);
-	redirectToPage();	
+	const trn = document.getElementById('trn').value;
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+	const form = document.getElementById('form');
+	
+	
+	localStorage.setItem("storedUser", stringify(trn));
+	localStorage.setItem("storedEmail", stringify(email));
+	localStorage.setItem("storedPassword", stringify(password));
+
+	if(checkInputs() != true){
+		window.alert("Registration Incomplete!");
+	}else{
+		redirectToPage();	
+	}
+
 });
 
 function redirectToPage() {
@@ -31,49 +43,59 @@ function reset(){
 	document.getElementById("password").value= '';
 }
 
-
+/*
 form.addEventListener('submit', e => {
 	e.preventDefault();
 	
 	checkInputs();
 });
+*/
 
 function checkInputs() {
-	
+	let complete = true;
 	const trnValue = trn.value.trim();
 	const emailValue = email.value.trim();
 	const passwordValue = password.value.trim();
 	const password2Value = password2.value.trim();
 	
-	if(usernameValue === '') {
+	if(trnValue === '') {
 		setErrorFor(trn, 'TRN cannot be blank');
+		complete= false;
 	} else {
 		setSuccessFor(trn);
 	}
 	
 	if(emailValue === '') {
 		setErrorFor(email, 'Email cannot be blank');
+		complete= false;
 	} else if (!isEmail(emailValue)) {
 		setErrorFor(email, 'Not a valid email');
+		complete = false;
 	} else {
 		setSuccessFor(email);
 	}
 	
 	if(passwordValue === '') {
 		setErrorFor(password, 'Password cannot be blank');
+		complete=false;
 	} else {
 		setSuccessFor(password);
 	}
 	
 	if(password2Value === '') {
 		setErrorFor(password2, 'Password2 cannot be blank');
+		complete = false;
 	} else if(passwordValue !== password2Value) {
 		setErrorFor(password2, 'Passwords does not match');
+		complete = false;
 	} else{
 		setSuccessFor(password2);
 	}
+
+	return complete;
 }
 
+/*
 //Stores trn as as registrationdata
 document.getElementById('form').addEventListener('submit', function (event) {
 	event.preventDefault();
@@ -85,6 +107,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
 		password2: document.getElementById('password2').value};
 	localStorage.setItem('registrationData', JSON.stringify(registrationData));
 });
+*/
 
 function setErrorFor(input, message) {
 	const formControl = input.parentElement;
