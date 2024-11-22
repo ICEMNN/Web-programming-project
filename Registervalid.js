567fb38554a6eadfd0569be76720c19a4c77fecb
ICEMNN <seangroves9@gmail.com>
2024-11-21 20:16:28 -0500

    added more fields to the register page

--- Registervalid.js
+++ Registervalid.js
@@ -3,7 +3,10 @@ const trn = document.getElementById('trn');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
-
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const dob = document.getElementById('dob');
+const sex = document.getElementById('sex');
 
 function stringify(item) {
     return JSON.stringify(item);
@@ -18,16 +21,25 @@ document.getElementById('form').addEventListener('submit', function(event) {
 	const email = document.getElementById('email').value;
 	const password = document.getElementById('password').value;
 	const form = document.getElementById('form');
-	
+	const firstName = document.getElementById('firstName').value;
+	const lastName = document.getElementById('lastName').value;
+	const dob = document.getElementById('dob').value;
+	const sex = document.getElementById('sex').value;
 	
 	localStorage.setItem("storedUser", stringify(trn));
 	localStorage.setItem("storedEmail", stringify(email));
 	localStorage.setItem("storedPassword", stringify(password));
+	localStorage.setItem("firstName",stringify(firstName));
+	localStorage.setItem("lastName",stringify(lastName));
+	localStorage.setItem("dob",stringify(dob));
+	localStorage.setItem("sex",stringify(sex));
+	
 
 	if(checkInputs() != true){
 		window.alert("Registration Incomplete!");
 	}else{
-		redirectToPage();	
+		redirectToPage();
+		calculateAge();	
 	}
 
 });
@@ -41,15 +53,10 @@ function reset(){
 	document.getElementById("trn").value= '';
 	document.getElementById("email").value= '';
 	document.getElementById("password").value= '';
+
 }
 
-/*
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	
-	checkInputs();
-});
-*/
+
 
 function checkInputs() {
 	let complete = true;
@@ -57,13 +64,49 @@ function checkInputs() {
 	const emailValue = email.value.trim();
 	const passwordValue = password.value.trim();
 	const password2Value = password2.value.trim();
+	const fNameValue = firstName.value.trim();
+	const lNameValue = lastName.value.trim(); 
+	const dateValue = dob.value;
+	const sexValue= sex.value;
 	
+
+
+	if(fNameValue === ''){
+		setErrorFor(firstName, 'Please enter your name');
+		complete=false;
+	}else{
+		setSuccessFor(firstName);
+	}
+
+	if(lNameValue === ''){
+		setErrorFor(lastName, 'Please enter your name');
+		complete=false;
+	}else{
+		setSuccessFor(lastName);
+	}
+
+	if(dateValue === ''){
+		setErrorFor(dateValue, 'Please enter your date of birth');
+		complete=false;
+	}else{
+		setSuccessFor(dob);
+	}
+
+	if(sexValue === ''){
+		setErrorFor(sexValue,'Please select your sex');
+		complete = false;
+	}else{
+		setSuccessFor(sex);
+	}
+
+
 	if(trnValue === '') {
 		setErrorFor(trn, 'TRN cannot be blank');
 		complete= false;
 	} else {
 		setSuccessFor(trn);
 	}
+
 	
 	if(emailValue === '') {
 		setErrorFor(email, 'Email cannot be blank');
@@ -125,8 +168,16 @@ function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
+//calculate the age of the person registering and stores it in local storage
+function calculateAge(){
+	const dateInput = document.getElementById("dob").value;
+	const birthDate = new Date(dateInput);
+	const currentDate = new Date();
+	const age = (new Date(currentDate - birthDate).getFullYear)
+	localStorage.setItem("age", stringify(age));
 
-
+	window.alert(`you are ${age} YEARS OLD`)
+}
 
 
 
